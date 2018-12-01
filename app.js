757a28194687ff740339ce6b6b6ef001a00de11f
upqode-bohdan <bogdan@upqode.com>
2018-12-01 20:11:44 +0200

    s3 l11

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-console.log('Result: ', notes.sumNumb(9, -2));
+console.log(_.isString(true));
+console.log(_.isString("Text"));
+
+let testArr = [1, "Text", 1, 2, 3, 1, 2, 5, 6, 7];
+let filteredArr = _.uniq(testArr);
+
+console.log(filteredArr);
+
+// console.log('Result: ', notes.sumNumb(9, -2));
