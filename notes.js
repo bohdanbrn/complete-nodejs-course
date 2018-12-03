c0e96bbe9c8c830ade895b25d6f333a5c123685c
upqode-bohdan <bogdan@upqode.com>
2018-12-04 00:22:04 +0200

    s3 l14

--- notes.js
+++ notes.js
@@ -1,4 +1,23 @@
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New note';
+
+var addNote = (title, body) => {
+    console.log('Adding note:', title, body);
+};
+
+var getAll = () => {
+    console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+    console.log('Getiing note: ', title);
+};
+
+var removeNote = (title) => {
+    console.log('Remove note: ', title);
 }
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
+};
