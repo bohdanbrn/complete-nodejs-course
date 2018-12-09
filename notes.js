3e854df0f633de62931669e1a0358b0fe37834ad
upqode-bohdan <bogdan@upqode.com>
2018-12-09 22:56:17 +0200

    s3 l17

--- notes.js
+++ notes.js
@@ -1,27 +1,32 @@
 
 const fs = require('fs');
 
+var fetchNotes = () => {
+    try {
+        var notesStr = fs.readFileSync('notes-data.json');
+        return JSON.parse(notesStr);
+    } catch (e) {
+        return [];
+    }
+};
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
 var addNote = (title, body) => {
-    var notes = [];
+    var notes = fetchNotes();
     var note = {
         title,
         body
     };
-
-    try {
-        var notesStr = fs.readFileSync('notes-data.json');
-        notes = JSON.parse(notesStr);
-    } catch (e) {
-        console.log('File "notes-data.json" does not exist');
-    }
-    
     var duplicateNotes = notes.filter((note) => note.title === title);
 
     if (duplicateNotes.length === 0) {
         notes.push(note);
-        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+        saveNotes(notes);
+        return note;
     }
-    
 };
 
 var getAll = () => {
