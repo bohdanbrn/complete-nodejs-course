f05f9ca8e11ac47dfc7be3bcf27d85834e7efa16
upqode-bohdan <bogdan@upqode.com>
2018-12-12 23:00:03 +0200

    s3 l19

--- app.js
+++ app.js
@@ -14,9 +14,7 @@ if (command === 'add') {
 
     if (note) {
         console.log('Note added');
-        console.log('-----');
-        console.log(`Title: ${note.title}`);
-        console.log(`Body: ${note.body}`);
+        notes.logNote(note);
     }
     else {
         console.log('Note is duplicated');
@@ -24,7 +22,13 @@ if (command === 'add') {
 } else if (command === 'list') {
     notes.getAll();
 } else if (command === 'read') {
-    notes.getNote(argv.title);
+    var note = notes.getNote(argv.title);
+    if (note) {
+        console.log('Note found');
+        notes.logNote(note);
+    } else {
+        console.log('Note not found');
+    }
 } else if (command === 'remove') {
     var noteRemoved = notes.removeNote(argv.title);
     var message = noteRemoved ? 'Note was removed' : 'Note not found';
--- notes.js
+++ notes.js
@@ -34,7 +34,9 @@ var getAll = () => {
 };
 
 var getNote = (title) => {
-    console.log('Getiing note: ', title);
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter(note => note.title === title);
+    return filteredNotes[0];
 };
 
 var removeNote = (title) => {
@@ -42,12 +44,19 @@ var removeNote = (title) => {
     var filteredNotes = notes.filter((note) => note.title !== title);
     saveNotes(filteredNotes);
 
-    return notes.length !== filteredNotes.length;
+    return notes.length === filteredNotes.length;
+}
+
+var logNote = (note) => {
+    console.log('-----');
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
 }
 
 module.exports = {
     addNote,
     getAll,
     getNote,
-    removeNote
+    removeNote,
+    logNote
 };
