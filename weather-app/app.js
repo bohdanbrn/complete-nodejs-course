88afba446ebeb7ba952cc26c352f58682e117812
upqode-bohdan <bogdan@upqode.com>
2018-12-23 23:13:44 +0200

    s4 l29

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,5 @@ request({
     url: 'http://www.mapquestapi.com/geocoding/v1/address?key=Ai05Ul0OA7ODe2jdgajLESdCSCGknADa&location=Lysynychi%20Ukraine',
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(JSON.stringify(body, undefined, 2));
 });
