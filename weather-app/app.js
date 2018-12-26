16bee6f47e15e35e47f27703f45766b9b6cd9517
upqode-bohdan <bogdan@upqode.com>
2018-12-26 23:27:16 +0200

    s4 l32

--- weather-app/app.js
+++ weather-app/app.js
@@ -20,11 +20,22 @@ request({
     url: `http://www.mapquestapi.com/geocoding/v1/address?key=Ai05Ul0OA7ODe2jdgajLESdCSCGknADa&location=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-    var location = body.results[0].providedLocation.location;
-    var lat = body.results[0].locations[0].latLng.lat;
-    var lng = body.results[0].locations[0].latLng.lng;
+    if (error) {
+        console.log('Unable to connect map service');
+    }
+    else if (body.info.statuscode !== 0) {
+        console.log('Illegal address for location');
+    }
+    else if (!body.results.length || !body.results[0].locations.length) {
+        console.log('Unable to find that address');
+    }
+    else {
+        var location = body.results[0].providedLocation.location;
+        var lat = body.results[0].locations[0].latLng.lat;
+        var lng = body.results[0].locations[0].latLng.lng;
 
-    console.log(`Location: ${location}`);
-    console.log(`Latitude: ${lat}`);
-    console.log(`Longitude: ${lng}`);
+        console.log(`Location: ${location}`);
+        console.log(`Latitude: ${lat}`);
+        console.log(`Longitude: ${lng}`);
+    }
 });
