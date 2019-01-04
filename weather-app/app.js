ac38caf241510e6888984415e4d8c2f00d3dfa26
upqode-bohdan <bogdan@upqode.com>
2019-01-04 22:50:58 +0200

    s4 l35

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,6 @@
 const yargs = require('yargs');
 const mapquest = require('./mapquest/mapquest');
-const darksky = require('./darksky/darksky');
+const weather = require('./weather/weather');
 
 
 const argv = yargs
@@ -19,15 +19,22 @@ const argv = yargs
 let mapquestKey = 'Ai05Ul0OA7ODe2jdgajLESdCSCGknADa';
 let darkSkyKey = '2b6789cb0c2f2d67555d2c52574f5e79';
 
-// mapquest.mapquestAddress(mapquestKey, argv.address, (errorMessage, results) => {
-//     if (errorMessage) {
-//         console.log(errorMessage);
-//     }
-//     else {
-//         console.log(JSON.stringify(results, undefined, 2));
-//     }
-// });
+mapquest.mapquestAddress(mapquestKey, argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
+    }
+    else {
+        console.log(results.address);
+        
+        weather.getWeather(darkSkyKey, results.lat, results.lng, (weatherErrorMessage, weatherResults) => {
+            if (weatherErrorMessage) {
+                console.log(weatherErrorMessage);
+            }
+            else {
+                console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}`);
+            }
+        });
 
+    }
+});
 
-let darkskyRes = darksky.darkskyWeather(darkSkyKey, 49.833277, 24.122925);
-console.log(darkskyRes);
