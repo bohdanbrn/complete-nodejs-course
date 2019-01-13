de00e9b3735b0be983b19a56353ce005fea85038
bohdanbrn <bohdanbrn@gmail.com>
2019-01-13 18:37:18 +0200

    s6 l53

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,18 +1,43 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', () => {
     let res = utils.add(33, 11);
 
-    if ( res != '44') {
-        throw new Error(`Expected 44, but got ${res}.`);
-    }
+    expect(res).toBe(44).toBeA('number');
 });
 
 
 it('should square number', () => {
-    let res = utils.square(2);
+    let res = utils.square(3);
 
-    if ( res != '4') {
-        throw new Error(`Expected 4, but got ${res}.`);
-    }
+    expect(res).toBe(9).toBeA('number');
 });
+
+// should verify first and last names are set
+// assert it includes firstName and lastName with proper values
+it('should set firstName and lastName with proper values', () => {
+    let user = {
+        location: 'Lviv',
+        age: 23
+    }
+    let res = utils.setName(user, 'Bohdan Baran');
+
+    expect(res).toInclude({
+        firstName: 'Bohdan',
+        lastName: 'Baran'
+    }).toBeA('object');
+});
+
+// it('should expect some values', () => {
+//     // expect(12).toNotBe(11);
+//     // expect({name: 'Bohdan'}).toEqual({name: 'Bohdan'});
+//     // expect([2,3,4]).toExclude(5);
+//     expect({
+//         name: "Bohdan",
+//         age: 23,
+//         location: 'Philadelphia',
+//     }).toExclude({
+//         age: 25
+//     })
+// });
