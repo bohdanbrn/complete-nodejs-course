9f97650df8ab66d05429403a6d1547ea85cbb19f
bohdanbrn <bohdanbrn@gmail.com>
2019-01-13 21:21:43 +0200

    s6 l58

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -3,28 +3,34 @@ const expect = require('expect');
 
 const app = require('./server').app;
 
-it('should return hello world response', (done) => {
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                error: 'Page not found.',
-                name: 'Todo App v1.0'
-            });
-        })
-        .end(done);
-});
+describe('Server', () => {
+    describe('GET /', () => {
+        it('should return hello world response', (done) => {
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'Page not found.',
+                        name: 'Todo App v1.0'
+                    });
+                })
+                .end(done);
+        });
+    });
 
-it('should return users data', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                name: "Bohdan",
-                age: 23
-            });
-        })
-        .end(done);
+    describe('GET /users', () => {
+        it('should return users data', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        name: "Bohdan",
+                        age: 23
+                    });
+                })
+                .end(done);
+        });
+    });
 });
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,57 +1,65 @@
 const expect = require('expect');
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-    let res = utils.add(33, 11);
-
-    expect(res).toBe(44).toBeA('number');
-});
-
-it('should asunc add two numbers', (done) => {
-    utils.assyncAdd(4, 3, (sum) => {
-        expect(sum).toBe(7).toBeA('number');
-        done();
+describe('Utils', () => {
+    describe('#add', () => {
+        it('should add two numbers', () => {
+            let res = utils.add(33, 11);
+        
+            expect(res).toBe(44).toBeA('number');
+        });
+        
+        it('should asunc add two numbers', (done) => {
+            utils.assyncAdd(4, 3, (sum) => {
+                expect(sum).toBe(7).toBeA('number');
+                done();
+            });
+        });
+        
     });
-});
-
-
-it('should square number', () => {
-    let res = utils.square(3);
-
-    expect(res).toBe(9).toBeA('number');
-});
-
-it('should assync square number', (done) => {
-    utils.assyncSquare(3, (square) => {
-        expect(square).toBe(9).toBeA('number');
-        done();
+    
+    describe('#square', () => {
+        it('should square number', () => {
+            let res = utils.square(3);
+        
+            expect(res).toBe(9).toBeA('number');
+        });
+        
+        it('should assync square number', (done) => {
+            utils.assyncSquare(3, (square) => {
+                expect(square).toBe(9).toBeA('number');
+                done();
+            });
+        });
     });
+    
+    describe('#fullName', () => {
+        // should verify first and last names are set
+        // assert it includes firstName and lastName with proper values
+        it('should set firstName and lastName with proper values', () => {
+            let user = {
+                location: 'Lviv',
+                age: 23
+            }
+            let res = utils.setName(user, 'Bohdan Baran');
+        
+            expect(res).toInclude({
+                firstName: 'Bohdan',
+                lastName: 'Baran'
+            }).toBeA('object');
+        });
+    });
+    
+    // it('should expect some values', () => {
+    //     // expect(12).toNotBe(11);
+    //     // expect({name: 'Bohdan'}).toEqual({name: 'Bohdan'});
+    //     // expect([2,3,4]).toExclude(5);
+    //     expect({
+    //         name: "Bohdan",
+    //         age: 23,
+    //         location: 'Philadelphia',
+    //     }).toExclude({
+    //         age: 25
+    //     })
+    // });    
 });
-
-// should verify first and last names are set
-// assert it includes firstName and lastName with proper values
-it('should set firstName and lastName with proper values', () => {
-    let user = {
-        location: 'Lviv',
-        age: 23
-    }
-    let res = utils.setName(user, 'Bohdan Baran');
-
-    expect(res).toInclude({
-        firstName: 'Bohdan',
-        lastName: 'Baran'
-    }).toBeA('object');
-});
-
-// it('should expect some values', () => {
-//     // expect(12).toNotBe(11);
-//     // expect({name: 'Bohdan'}).toEqual({name: 'Bohdan'});
-//     // expect([2,3,4]).toExclude(5);
-//     expect({
-//         name: "Bohdan",
-//         age: 23,
-//         location: 'Philadelphia',
-//     }).toExclude({
-//         age: 25
-//     })
-// });
