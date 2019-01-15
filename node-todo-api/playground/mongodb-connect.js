e5862d53ee7f89ec316fe76d9694beae09e77fd8
bohdanbrn <bohdanbrn@gmail.com>
2019-01-15 22:55:57 +0200

    s7 l64

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
 // Connection URL
 const url = 'mongodb://localhost:27017';
@@ -14,22 +15,22 @@ client.connect(function(err) {
     if (err) {
         return console.log('Unable to connect to MongoDB server');
     }
-
     console.log("Connected successfully to server");
   
-    const db = client.db(dbName);
+    // const db = client.db(dbName);
 
-    db.collection('Users').insertOne({
-        name: 'Bohdan',
-        age: '23',
-        location: 'Lviv'
-    }, (err, result) =>  {
-        if (err) {
-            return console.log('Unable to insert user', err);
-        }
+    // db.collection('Users').insertOne({
+    //     name: 'Bohdan',
+    //     age: '23',
+    //     location: 'Lviv'
+    // }, (err, result) =>  {
+    //     if (err) {
+    //         return console.log('Unable to insert user', err);
+    //     }
 
-        console.log(JSON.stringify(result.ops, undefined, 2));
-    });
+    //     // console.log(JSON.stringify(result.ops, undefined, 2));
+    //     console.log(result.ops[0]._id.getTimestamp());
+    // });
   
     client.close();
   });
