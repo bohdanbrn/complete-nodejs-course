a55c09edd9d5faa27cc8543f5914ad321267f63c
bohdanbrn <bohdanbrn@gmail.com>
2019-01-26 20:09:06 +0200

    s7 l78(GET /todos/:id)

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,9 +1,10 @@
-let express = require('express');
-let bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-let {mongoose} = require('./db/mongoose');
-let {Todo} = require('./models/todo');
-let {User} = require('./models/user');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 let app = express();
 
@@ -28,6 +29,26 @@ app.get('/todos', (req, res) => {
         res.status(400).send(e);
     })
 });
+
+// GET /todos/1233254
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.send();
+        }
+        
+        res.send({todo});
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
+});
+
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
