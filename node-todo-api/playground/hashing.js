28e288940ff99a9a3d8e788af45c2e8dbef8d3b3
bohdanbrn <bohdanbrn@gmail.com>
2019-02-04 22:29:23 +0200

    s8 l92 (Hashing password)

--- node-todo-api/playground/hashing.js
+++ node-todo-api/playground/hashing.js
@@ -1,16 +1,34 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-    id: 6747
-}
+let password = '123abc!';
 
-let secretKey = '_123!abdf#hf}{hc"';
-let token = jwt.sign(data, secretKey);
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
 
-let decoded = jwt.verify(token, secretKey);
-console.log('decoded', decoded);
+let hashedPassword = '$2a$10$VY4GBOuHDRQluK0KuBpkXOTyIH.NirIH5A7WNyl3aMP66O8NZLAsK';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+
+// let data = {
+//     id: 6747
+// }
+
+// let secretKey = '_123!abdf#hf}{hc"';
+// let token = jwt.sign(data, secretKey);
+// console.log(token);
+
+// let decoded = jwt.verify(token, secretKey);
+// console.log('decoded', decoded);
+
+
 
 // let message = "I'm user number 3";
 // let hash = SHA256(message).toString();
