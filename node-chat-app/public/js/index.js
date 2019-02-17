09a3227930f1b976156143e7bf2dcad3c7dbb895
bohdanbrn <bohdanbrn@gmail.com>
2019-02-17 15:29:39 +0200

    s9 l110 (Great new user and alert others)

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'User',
-        text: 'Yes, of course'
-    });
 });
 
 socket.on('disconnect', function() {
--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -16,13 +16,25 @@ io.on('connection', (socket) => {
     console.log('New user connected');
 
     socket.emit('newMessage', {
-        from: 'Bohdan',
-        text: 'Hey. Can you meet up at 6:00?',
-        createdAt: '123123'
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
     });
 
     socket.on('createMessage', (message) => {
         console.log('create Message', message);
+
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
