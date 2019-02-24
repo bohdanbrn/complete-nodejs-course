0e2caa8aaa49995dc83f46ca09be2d828217e94b
bohdanbrn <bohdanbrn@gmail.com>
2019-02-24 22:30:58 +0200

    s9 l118 (Printing Message Timestamps)

--- node-chat-app/playground/time.js
+++ node-chat-app/playground/time.js
@@ -8,6 +8,12 @@ const moment = require('moment');
 // date.add(100, 'year').subtract(9, 'months');
 // console.log(date.format('MMM Do, YYYY'));
 
-let date = moment();
-console.log(date.format('hh:mm a'));
-console.log(date.subtract().format('h:mm a'));
+// let date = moment();
+// console.log(date.format('hh:mm a'));
+// console.log(date.subtract().format('h:mm a'));
+
+let someTimestamp = moment().valueOf();
+console.log(someTimestamp);
+
+// let date = moment(createdAt);
+// console.log(date.format('hh:mm a'));
