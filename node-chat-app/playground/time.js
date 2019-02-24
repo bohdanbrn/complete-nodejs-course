7cab6e5d8153580f5448545b3a938ffccaa580e8
bohdanbrn <bohdanbrn@gmail.com>
2019-02-24 22:13:27 +0200

    s9 l117 (Moment)

---
+++ node-chat-app/playground/time.js
@@ -0,0 +1,13 @@
+const moment = require('moment');
+
+// let date = new Date();
+// let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+// console.log(months[date.getMonth()]);
+
+// let date = moment();
+// date.add(100, 'year').subtract(9, 'months');
+// console.log(date.format('MMM Do, YYYY'));
+
+let date = moment();
+console.log(date.format('hh:mm a'));
+console.log(date.subtract().format('h:mm a'));
